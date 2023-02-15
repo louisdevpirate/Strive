0e025a75f95e3e956a621a1f6516c735734ab8c5
Louis Dole <louis.dole@live.fr>
2023-02-15 20:40:49 +0100

    Peauffinement de la page Home en version mobile first

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_intro')]
+    public function intro(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/intro.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig');
     }
 }
